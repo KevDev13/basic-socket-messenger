1c609c5ccf42c5710000c6b491582e3a90304bec
Kevin Garner <kevin@kgar.net>
2021-05-09 21:31:39 -0500

    more work on initial prototype

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::{ Arg, App };
 
 const APP_NAME_SHORT: &str = "BSM";
 const APP_NAME_LONG: &str = "Basic Socket Messenger";
+const MAX_MSG_SIZE: i16 = 128;
 
 fn main() {
     let args = App::new(format!("{} - {}", APP_NAME_SHORT, APP_NAME_LONG))
@@ -17,11 +18,31 @@ fn main() {
 
     let ip_address = args.value_of("ip").unwrap();
 
-    let mut socket = UdpSocket::bind(ip_address).expect("Could not bind to IP address");
+    {
+        let mut socket = UdpSocket::bind(ip_address).expect("Could not bind to IP address");
 
-    println!("{}: Bound to IP {}", APP_NAME_SHORT, ip_address);
-    println!("{}: Enter message to send. Enter blank message to close", APP_NAME_SHORT);
-    let mut input = String::new();
-    let bytes = std::io::stdin().read_line(&mut input).expect("Error reading input");
-    
+        println!("{}: Bound to IP {}", APP_NAME_SHORT, ip_address);
+
+        loop {
+            println!("{}: Enter message to send. Enter blank message to close", APP_NAME_SHORT);
+            let mut input = String::new();
+            let bytes = std::io::stdin().read_line(&mut input).expect("Error reading input");
+            
+            // remove EOL characters
+            if let Some('\n') = input.chars().next_back() {
+                input.pop();
+            } else if let Some('\r') = input.chars().next_back() {
+                input.pop();
+            }
+
+            if input == "" {
+                break;
+            }
+            print!("sending...");
+            socket.send_to(input.as_bytes(), &ip_address);
+            println!(" sent! Awaiting response");
+            let mut buf = [0; MAX_MSG_SIZE];
+        }
+    } // socket closed
+    println!("Connection to {} closed", ip_address);
 }
